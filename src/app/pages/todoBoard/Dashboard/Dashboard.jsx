c4d3bf516377f32d84e8041eeb99da88e38174f2
rsaldana <ruben.saldana-ruiz@capgemini.com>
2023-02-13 15:43:23 +0100

    Refactor & first mock

--- src/app/pages/todoBoard/Dashboard/Dashboard.jsx
+++ src/app/pages/todoBoard/Dashboard/Dashboard.jsx
@@ -1,26 +1,58 @@
-
-import TodoColumn from './TodoColumn/TodoColumn';
-import InProgressColumn from './InProgressColumn/InProgressColumn';
-import LockedColumn from './LockedColumn/LockedColumn';
-import DoneColumn from './DoneColumn/DoneColumn';
+import { useEffect, useState } from 'react';
+import Task from '../Task/Task';
 import './Dashboard.scss';
-
+import listMock from './tasksMock';
 
 const Dashboard = () => {
+
+  //leer desde backend la list de tasks
+
+  const [todo, setTodo] = useState([]);
+  const [inProgress, setInProgress] = useState([]);
+  const [locked, setLocked] = useState([]);
+  const [done, setDone] = useState([]);
+
+	useEffect(() => {
+		setTodo(listMock.todo);
+    setInProgress(listMock.inProgress);
+    setLocked(listMock.locked);
+    setDone(listMock.done);
+	}, []);
+
 	return (
 		<div className={'tb-c-dashboard'}>
       <div className={'tb-c-dashboard__columns'}>
         <div className={'tb-c-dashboard__columns__column'}>
-          <TodoColumn />
+          <h1 className={'tb-c-dashboard__columns__column__title'}> To do {todo.length}</h1>
+          <div className={'tb-c-dashboard__columns__column__card'}>
+            {todo && todo.map((task) => {
+              return <Task key={task.id} taskInfo={task}/>;
+            })}
+          </div>
         </div>
         <div className={'tb-c-dashboard__columns__column'}>
-          <InProgressColumn />
+          <h1 className={'tb-c-dashboard__columns__column__title'}> In Progress {inProgress.length}</h1>
+          <div className={'tb-c-dashboard__columns__column__card'}>
+            {inProgress && inProgress.map((task) => {
+              return <Task key={task.id} taskInfo={task}/>;
+            })}
+          </div>
         </div>
         <div className={'tb-c-dashboard__columns__column'}>
-          <LockedColumn />
+          <h1 className={'tb-c-dashboard__columns__column__title'}> Locked {locked.length}</h1>
+          <div className={'tb-c-dashboard__columns__column__card'}>
+            {locked && locked.map((task) => {
+              return <Task key={task.id} taskInfo={task}/>;
+            })}
+          </div>
         </div>
         <div className={'tb-c-dashboard__columns__column'}>
-          <DoneColumn />
+          <h1 className={'tb-c-dashboard__columns__column__title'}> Done {done.length}</h1>
+          <div className={'tb-c-dashboard__columns__column__card'}>
+            {done && done.map((task) => {
+              return <Task key={task.id} taskInfo={task}/>;
+            })}
+          </div>
         </div>
       </div>
     </div>
--- src/app/pages/todoBoard/Task/Task.jsx
+++ src/app/pages/todoBoard/Task/Task.jsx
@@ -2,30 +2,20 @@
 import './Task.scss';
 
 
-const Dashboard = () => {
-
-  const task = {
-    name: 'My Name',
-    description: 'This is my first example of the task card',
-    type: 'university',
-    priority: 5,
-    created: '5-08-2020',
-    deadline: '15-03-2023',
-    column: 'todo',
-  };
+const Dashboard = (props) => {
 
 	return (
 		<div className={'tb-c-task'}>
-      <h1 className={'tb-c-task__name'}> {task.name} </h1>
+      <h1 className={'tb-c-task__name'}> {props.taskInfo.name} </h1>
       <div className={'tb-c-task__info'}>
-        <p className={'tb-c-task__info__date'}> Created: {task.created} </p>
-        <p className={'tb-c-task__info__date'}> Deadline: {task.deadline} </p>
+        <p className={'tb-c-task__info__date'}> Created: {props.taskInfo.created} </p>
+        <p className={'tb-c-task__info__date'}> Deadline: {props.taskInfo.deadline} </p>
         <div className={'tb-c-task__info__type-and-prio'}> 
           <div className={'tb-c-task__info__type-and-prio__type'}>
-            <p className={'tb-c-task__info__type-and-prio__type__title'}> {task.type} </p>
+            <p className={'tb-c-task__info__type-and-prio__type__title'}> {props.taskInfo.type} </p>
           </div>
           <div className={'tb-c-task__info__type-and-prio__priority'}>
-            <p className={'tb-c-task__info__type-and-prio__priority__title'}> {task.priority} </p>
+            <p className={'tb-c-task__info__type-and-prio__priority__title'}> {props.taskInfo.priority} </p>
           </div>
         </div>
       </div>
